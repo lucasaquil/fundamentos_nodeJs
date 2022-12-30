1352991def1672438274c5a0dc6ea5960a45ca70
Luc@s <lucasaquilapereira@gmail.com>
2022-12-30 12:52:36 -0300

    Mais exercicios de JS

---
+++ console.js
@@ -0,0 +1,12 @@
+// log == registro
+console.log(0);
+console.log(true);
+console.log("Receba");
+
+// Tratamento de erro
+
+//console.error("Olá, se você está lendo isso, algo conteceu, e não foi bom. :[]")
+
+//Exercícios
+console.error(new Error("Olá, se você está lendo isso, algo conteceu, e não foi bom. :[]"))
+
