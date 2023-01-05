56bd0c2308b611085667f2f60c2b88ff9d5d8aaf
Luc@s <lucasaquilapereira@gmail.com>
2023-01-05 11:50:14 -0300

    Compreendendo maneiras de manipular arrays

---
+++ array-concatenacao.js
@@ -0,0 +1,6 @@
+const alunosJS = ["Pitágoras", "Mano", "Platão"];
+
+const alunosPY = ["Ted", "Holmes", "Menino Ney"];
+
+const palestra = alunosJS.concat(alunosPY);
+console.log(palestra);
---
+++ array-duas-dimensoes.js
@@ -0,0 +1,12 @@
+const alunos = ["Cornelius", "Ja", "Jay", "Kratos"];
+const mediasAlunos = [5, 6, 8, 4];
+
+const listaIntegrada = [alunos, mediasAlunos];
+console.log(`Kratos repetiu, esse individuo tirou ${listaIntegrada[1][3]} em Lingua Portuguesa`);
+
+// Desafio
+const arrayOriginal = ["Maria", "Carlos", "Eduardo", "Samanta"];
+const arrayConcat = arrayOriginal.concat("André", "Fernanda");
+
+console.log(arrayConcat);
+console.log(arrayOriginal);
